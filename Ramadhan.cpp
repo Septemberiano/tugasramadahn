0dab1a2ba0b5037b71a8dbad3a1e5c8a5943fec3
adityaseptemberiano <ponsipon249@gmail.com>
2025-03-09 19:12:41 +0700

    update tipis

--- Ramadhan.cpp
+++ Ramadhan.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 using namespace std;
+struct tanggal
+{
+    int hari;
+    int bulan;
+    int tahun;
+};
 
+struct makanan
+{
+    int jumlah;
+    string namamenu;
+    tanggal tgl;
+};
+makanan takjil;
+struct data
+{
+    int nim;
+    string nama;
+    string jurusan;
+};
+data pengambil;
+void inputmakan(){
+    cout<<"\tInput Takjil Masjid Sabrini\n";
+    cout<<"Masukan tanggal :";cin>>takjil.tgl.hari;
+    cout<<"masukan Bulan :";cin>>takjil.tgl.bulan;
+    cout<<"masukan tahun :";cin>>takjil.tgl.tahun;
+
+}
 int main() {
     
     return 0;
